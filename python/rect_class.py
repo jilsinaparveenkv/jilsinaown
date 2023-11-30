844522799440e83fd8dbe9c9417028769d7c319a
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2023-11-30 16:35:27 +0530

    Add files via upload

---
+++ python/rect_class.py
@@ -0,0 +1,21 @@
+#create rectangles class with attributes length and breadth and methods to find area and peremeter.compare two rectangle objects by their area.
+
+class Rectangle():
+
+ def __init__(self, l, w):
+     self.length = l
+     self.breadth = w
+ def rectangle_area(self):
+     return self.length*self.breadth
+ def rectangle_peri(self):
+     return 2* (self.length+self.breadth)
+rectangle1 = Rectangle(12, 10)
+rectangle2 = Rectangle(6, 5)
+print("Area1 = ",rectangle1.rectangle_area())
+print("Area2 = ",rectangle2.rectangle_area())
+print("Perimeter1 = ",rectangle1.rectangle_peri())
+print("Perimeter2 = ",rectangle2.rectangle_peri())
+if (rectangle1.rectangle_area()==rectangle2.rectangle_area()):
+ print("Areas are Equal")
+else:
+ print("Areas are not Equal")
