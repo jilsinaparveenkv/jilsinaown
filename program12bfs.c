ec056712e91d971d7981eb64b6356dcadfaa57b8
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2023-12-13 12:41:05 +0530

    Add files via upload

---
+++ program12bfs.c
@@ -0,0 +1,197 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+struct AdjList
+{
+	int key;
+	struct AdjList *next;
+};
+struct AdjList *Vtx[5];
+
+
+
+
+
+
+int queue[25];
+int rear=-1,front=-1;
+int add_item;
+int temp;
+int vtx[5];
+
+int E,E1,E2;
+int k;
+int type;
+int graph[5][5];
+int visit[5]={0,0,0,0,0};
+//int graph[5][5]={{0,1,1,0,0},{1,0,0,1,1},{1,0,0,1,0},{0,1,1,0,1},{0,1,0,1,0}};
+int v;
+
+void getMatrix()
+{
+	int i,j;
+	for(i=0; i<v; i++)
+	{
+		for(j=0; j<v; j++)
+		{
+			scanf("%d",&graph[i][j]);
+		}
+	}
+}
+void createList(int v,int E)
+{
+//initializtion
+	int i;
+	struct AdjList *newnode *ptr;
+	for(i=0;i<v;i++)
+	vtx[i]=NULL;
+	printf("Enter the edges:");
+	for(int i=0; i<E; i++)
+	
+
+	newnode=(struct AdjList *)malloc(sizeof(struct AdjList));
+	newnode->key=E;
+	newnode->next=NULL;
+	if(vtx[v]==NULL)
+	{
+		vtx[v]=newnode;
+	}
+	else
+	{
+	ptr=vtx[v];
+	while(ptr->next !=NULL)
+	{
+	ptr=ptr->next;
+	ptr->next=newnode;
+	}
+	}
+}
+	
+void createMatrix()
+{
+	//initialization
+	int i,j;
+	int E1,E2;
+	for(i=0; i<v; i++)
+	{
+		for(j=0; j<v; j++)
+		{
+			graph[i][j]=0;
+		}
+	}
+	for(i=0; i<E; i++)
+	{
+		scanf("%d",&E1);
+		scanf("%d",&E2);
+		graph[E1][E2]=1;
+		if(type==2)
+		graph[E2][E1]=1;
+	}
+}
+
+void enqueue(int add_item)
+{
+ 	if(rear==25-1)
+	{
+		printf("\n queue is overflow.\n");
+	}
+	if(rear<25)
+	{
+		if(rear==-1 && front==-1)
+		{ 
+			front=rear=0;
+			queue[rear]=add_item;
+		}
+
+		else
+ 		{
+		rear=rear+1;
+		queue[rear]=add_item;
+		}
+	}
+}  
+int dequeue()
+{
+   if(front==-1 )
+   {
+     printf("\n queue is underflow.\n");
+   }
+  else if(front==rear)
+  {
+    
+    temp=queue[front];
+    front=-1;
+    rear=-1;
+  }
+  else
+   {
+     
+     temp=queue[front];
+     front=front+1;
+   }
+    return(temp);
+    
+}
+int isVisited(int vtx)
+{
+	return(visit[vtx]);
+}
+void bfs(int s)
+{
+        int vtx;
+	enqueue(s);
+	while(front!=-1 && rear!=-1)
+	{
+		vtx=dequeue();
+		if(!(isVisited(vtx)))
+		{
+			visit[vtx]=1;
+			printf("%d\n",vtx);
+			for(int i=0;i<5;i++)
+			{
+				if(graph[vtx][i]==1)
+					enqueue(i);
+			}
+		} 
+	}
+}
+		
+int main()
+{
+	printf("Enter the number of vertices:");
+	scanf("%d",&v);
+	//getMatrix(v);
+	printf("Enter the type of graph(enter 1 for directed or 2 for undirected graph):");
+	scanf("%d",&type);
+	
+	printf("Enter the number of edges:");
+	scanf("%d",&E);
+	for(i=0; i<E; i++)
+	{
+		scanf("%d",&E1);
+		scanf("%d",&E2);
+
+	createList(E1,E2);
+	if(type==2)
+	createList(E1,E2);
+	}
+	//createMatrix(v,E,type);
+	
+	//int i,j,k;
+	//for (i=0;i<5;++i)
+	//{
+	//	for(j=0;j<5;++j)
+	//	{
+		//	printf("%d ",graph[i][j]);
+		//}
+		//printf("\n");
+	//}
+	printf("source vertex :");
+        scanf("%d",&k);
+        bfs(k);
+}
+
+	
+	
+
+	
